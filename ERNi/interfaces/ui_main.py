8aca6c51838bcf807817675f20548b67d00d1514
Zhang, Yuxuan <zhangy6@ornl.gov>
2017-10-31 17:54:23 -0400

    skeleton

--- ERNi/interfaces/ui_main.py
+++ ERNi/interfaces/ui_main.py
@@ -1,9 +1,10 @@
-from PyQt5 import QtGui, QtWidgets  # Import the PyQt4 module we'll need
+from PyQt5 import QtCore, QtGui,  QtWidgets  # Import the PyQt4 module we'll need
 import sys  # We need sys so that we can pass argv to QApplication
 import ui_mainWindow  # This file holds our MainWindow and all design related things
-
+import os
 
 # it also keeps events etc that we defined in Qt Designer
+
 
 class MyApp(QtWidgets.QMainWindow, ui_mainWindow.Ui_MainWindow):
     def __init__(self):
@@ -14,6 +15,17 @@ class MyApp(QtWidgets.QMainWindow, ui_mainWindow.Ui_MainWindow):
         super(self.__class__, self).__init__()
         self.setupUi(self)  # This is defined in design.py file automatically
         # It sets up layout and widgets that are defined
+        self.data_import.clicked.connect(self.browse_folder)
+
+    def browse_folder(self):
+        self.listWidget.clear()  # In case there are any existing elements in the list
+        directory = QtWidgets.QFileDialog.getExistingDirectory(self, "Pick a folder")
+        # execute getExistingDirectory dialog and set the directory variable to be equal
+        # to the user selected directory
+
+        if directory:  # if user didn't pick a directory don't continue
+            for file_name in os.listdir(directory):  # for all files, if any, in the directory
+                self.QListWidget.addItem(file_name)  # add file to the listWidget
 
 
 def main():
