070df4d70c3c947d0de0ff2439dcdb3df4471611
Zhang, Yuxuan <zhangy6@ornl.gov>
2017-11-01 12:13:58 -0400

    skeleton

--- ERNi/interfaces/ui_main.py
+++ ERNi/interfaces/ui_main.py
@@ -1,4 +1,4 @@
-from PyQt5 import QtCore, QtGui,  QtWidgets  # Import the PyQt4 module we'll need
+from PyQt5 import QtCore, QtGui,  QtWidgets  # Import the PyQt5 module we'll need
 import sys  # We need sys so that we can pass argv to QApplication
 import ui_mainWindow  # This file holds our MainWindow and all design related things
 import os
@@ -37,3 +37,5 @@ def main():
 
 if __name__ == '__main__':  # if we're running file directly and not importing it
     main()  # run the main function
+
+
